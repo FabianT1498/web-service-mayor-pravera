87cfcbe6e70aec3f01927bf620a05c400720c701
Fabian Trillo <fabiantrillo1498@gmail.com>
2022-05-20 09:09:27 -0400

    se asigno un id unico a cada formulario de cierre

--- resources/js/pages/cash-register/index.js
+++ resources/js/pages/cash-register/index.js
@@ -7,8 +7,11 @@ export default {
         dateRangePicker: document.querySelector('#date_range_picker'),
         pagesLinksContainer: document.querySelector('#pages_links_container'),
         formFilter: document.querySelector('#form_filter'),
-        pageInput: document.querySelector('#page')
+        pageInput: document.querySelector('#page'),
+        acceptCashRegisterCloseBtn: document.querySelector('#accept-cash-register-close'),
+        closeCashRegisterCloseBtns: document.querySelectorAll('button[data_cash_register_close_id]')
     },
+    lastClickedCloseBtnID: -1,
     init(){
         Object.assign(Datepicker.locales, es);
 
@@ -29,11 +32,26 @@ export default {
             }
         }
 
+        let handleClickCashRegisterCloseBtn = function(lastID){
+            return (event) => {
+                lastID = event.target.closest('button').getAttribute('data_cash_register_id');
+                // form.submit();
+            }
+        }
+
+        let handleClickAcceptCashRegisterBtn = (lastID) => {
+            return function(event) {
+                let form = document.querySelector('#close-cash-register-form-' . lastID);
+                console.log(form)
+                // form.submit();
+            }
+        }
+        
+        /** Filtrar registros  */
         dateRangePicker.datepickers[0].element.addEventListener('hide', changeDateEventHandlerWrapper(this.DOMElements.formFilter));
         dateRangePicker.datepickers[1].element.addEventListener('hide', changeDateEventHandlerWrapper(this.DOMElements.formFilter));
-
         this.DOMElements.formFilter.addEventListener('change', changeWrapper(this.DOMElements.formFilter));
-
+    
         if (this.DOMElements.pagesLinksContainer){
             this.DOMElements.pagesLinksContainer.addEventListener('click', function(pageInput, form){
                 return (e) => {
@@ -49,5 +67,15 @@ export default {
                 }
             }(this.DOMElements.pageInput, this.DOMElements.formFilter));
         }
+
+
+        console.log(this.DOMElements.closeCashRegisterCloseBtns);
+
+        closeCashRegisterCloseBtns.foreach(function(){
+            item.addEventListener('click', handleClickCashRegisterCloseBtn)
+        })
+
+        /** Cierre de caja */
+        this.DOMElements.acceptCashRegisterCloseBtn.addEventListener('click', handleClickAcceptCashRegisterBtn(this.lastClickedCloseBtnID))        
     }
 }
