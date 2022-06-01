cd78f6dd3b43f029bd0870dc33190dbe3a7ca2c6
Fabian Trillo <fabiantrillo1498@gmail.com>
2022-06-01 15:55:58 -0400

    fix: se ha arreglado los estilos del boton flotante para las notas

--- resources/views/pages/cash-register/create.blade.php
+++ resources/views/pages/cash-register/create.blade.php
@@ -6,9 +6,17 @@
 
 @section('main')
     <div class="sticky right-0 top-0 flex flex-row justify-end items-center w-full h-auto">
-        <button type="button" data-modal-toggle="notes-modal" class="absolute top-4 mr-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-centerdark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <button type="button"  data-tooltip-target="open-note-modal"  data-modal-toggle="notes-modal" class="absolute top-4 mr-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm h-12 w-12 flex justify-center items-center  items-centerdark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <i class="fas fa-sticky-note font-semibold text-white text-xl"></i>
         </button>
+        <div 
+            id="open-note-modal"
+            role="tooltip" 
+            class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
+        >
+            Abrir notas
+            <div class="tooltip-arrow" data-popper-arrow></div>
+        </div> 
     </div>
     <form id="form" class="px-20" autocomplete="off" method="POST" action="{{ route('cash_register.store') }}">
         @csrf
