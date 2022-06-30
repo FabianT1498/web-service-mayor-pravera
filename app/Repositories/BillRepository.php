107d1f34bc1c80190b25da833b44d6a9a8037839
Fabian Trillo <fabiantrillo1498@gmail.com>
2022-06-30 16:04:49 -0400

    feat: se ha adaptado el factor de venta en la consulta de los vueltos

--- app/Repositories/BillRepository.php
+++ app/Repositories/BillRepository.php
@@ -15,12 +15,6 @@ class BillRepository implements BillRepositoryInterface
             ? "CAST(SAFACT.FechaE as date) = ?"
             : "CAST(SAFACT.FechaE as date) BETWEEN CAST(? as date) AND CAST(? as date)";
 
-        $factors = DB::connection('saint_db')
-            ->table('SAFACT')
-            ->selectRaw("ROUND(MAX(SAFACT.Factor), 2) as MaxFactor, CAST(SAFACT.FechaE as date) as FechaE")
-            ->whereRaw($interval_query, $queryParams)
-            ->groupByRaw("CAST(SAFACT.FechaE as date)");
-
         $egivales_sub_query = "(SELECT FactEmi, MontoDiv FROM EGIVALES WHERE EGIVALES.Estado = 'V' AND EGIVALES.FactUso = '') AS EGIVALES";
 
         return DB
@@ -28,14 +22,11 @@ class BillRepository implements BillRepositoryInterface
             ->table('SAFACT')
             ->selectRaw("MAX(SAFACT.CodUsua) as CodUsua, CAST(SAFACT.FechaE as date) as FechaE, MAX(SAFACT.NumeroD) as NumeroD, 
                 CAST(ROUND(SUM(COALESCE(EGIVALES.MontoDiv, 0) * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoDivEfect,
-                CAST(ROUND(SUM(COALESCE(EGIVALES.MontoDiv, 0) * FactorHist.MaxFactor * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoBsEfect,
+                CAST(ROUND(SUM(COALESCE(EGIVALES.MontoDiv, 0) * SAFACT.FactorV * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoBsEfect,
                 CAST(ROUND(SUM(COALESCE(TBL_REG_PagoM.MontoD, 0) * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoDivPM,
                 CAST(ROUND(SUM(COALESCE(TBL_REG_PagoM.MontoBs, 0) * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoBsPM,
-                MAX(FactorHist.MaxFactor) as Factor
+                MAX(SAFACT.FactorV) as Factor
             ")
-            ->joinSub($factors, 'FactorHist', function($query){
-                $query->on(DB::raw("CAST(SAFACT.FechaE AS date)"), '=', "FactorHist.FechaE");
-            })
             ->leftJoin(DB::raw($egivales_sub_query), 'EGIVALES.FactEmi', '=', 'SAFACT.NumeroD')
             ->leftJoin('TBL_REG_PagoM', 'TBL_REG_PagoM.NumeroD', '=', 'SAFACT.NumeroD')
             ->whereRaw("SAFACT.TipoFac IN ('A', 'B') AND SAFACT.CodUsua IN ('CAJA1', 'CAJA2', 'CAJA3', 'CAJA4', 'CAJA5',
@@ -58,12 +49,6 @@ class BillRepository implements BillRepositoryInterface
             ? "CAST(SAFACT.FechaE as date) = ?"
             : "CAST(SAFACT.FechaE as date) BETWEEN CAST(? as date) AND CAST(? as date)";
 
-        $factors = DB::connection('saint_db')
-            ->table('SAFACT')
-            ->selectRaw("ROUND(MAX(SAFACT.Factor), 2) as MaxFactor, CAST(SAFACT.FechaE as date) as FechaE")
-            ->whereRaw($interval_query, $queryParams)
-            ->groupByRaw("CAST(SAFACT.FechaE as date)");
-
         $egivales_sub_query = "(SELECT FactEmi, MontoDiv FROM EGIVALES WHERE EGIVALES.Estado = 'V' AND EGIVALES.FactUso = '') AS EGIVALES";
 
         return DB
@@ -71,13 +56,10 @@ class BillRepository implements BillRepositoryInterface
             ->table('SAFACT')
             ->selectRaw("MAX(SAFACT.CodUsua) as CodUsua, CAST(SAFACT.FechaE as date) as FechaE, 
                 CAST(ROUND(SUM(COALESCE(EGIVALES.MontoDiv, 0) * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoDivEfect,
-                CAST(ROUND(SUM(COALESCE(EGIVALES.MontoDiv, 0) * FactorHist.MaxFactor * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoBsEfect,
+                CAST(ROUND(SUM(COALESCE(EGIVALES.MontoDiv, 0) * SAFACT.FactorV * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoBsEfect,
                 CAST(ROUND(SUM(COALESCE(TBL_REG_PagoM.MontoD, 0) * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoDivPM,
                 CAST(ROUND(SUM(COALESCE(TBL_REG_PagoM.MontoBs, 0) * SAFACT.Signo), 2) AS decimal(10, 2)) as MontoBsPM
             ")
-            ->joinSub($factors, 'FactorHist', function($query){
-                $query->on(DB::raw("CAST(SAFACT.FechaE AS date)"), '=', "FactorHist.FechaE");
-            })
             ->leftJoin(DB::raw($egivales_sub_query), 'EGIVALES.FactEmi', '=', 'SAFACT.NumeroD')
             ->leftJoin('TBL_REG_PagoM', 'TBL_REG_PagoM.NumeroD', '=', 'SAFACT.NumeroD')
             ->whereRaw("SAFACT.TipoFac IN ('A', 'B') AND SAFACT.CodUsua " . $user_params . " AND " .
