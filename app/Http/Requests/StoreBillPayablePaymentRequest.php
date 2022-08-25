88e8ad8de8fb7dc762e2147b7e331744a3bfc344
Fabian Trillo <fabiantrillo1498@gmail.com>
2022-08-25 11:17:27 -0400

    feat: se ha agregado la validacion para determinar si una factura tiene una tasa, antes de almacenar el pago

--- app/Http/Requests/StoreBillPayablePaymentRequest.php
+++ app/Http/Requests/StoreBillPayablePaymentRequest.php
@@ -10,6 +10,7 @@ use App\Http\Traits\AmountCurrencyTrait;
 
 use App\Rules\BadFormattedAmount;
 use App\Rules\BillPayableExists;
+use App\Rules\BillPayableHasTasa;
 
 use App\Repositories\BillsPayableRepository;
 
@@ -38,10 +39,11 @@ class StoreBillPayablePaymentRequest extends FormRequest
         $total_rules = ['required', new BadFormattedAmount, 'gt:0'];
 
         $bill_payable_exists_validation =  (new BillPayableExists($repo))->setData(['nro_doc' => $this->nro_doc, 'cod_prov' => $this->cod_prov]);
-        
+        $bill_payable_has_tasa = (new BillPayableHasTasa())->setData(['nro_doc' => $this->nro_doc, 'cod_prov' => $this->cod_prov]);
+
         $rules = [
-            'nro_doc' => ['required', $bill_payable_exists_validation],
             'cod_prov' => ['required'],
+            'nro_doc' => ['required', $bill_payable_exists_validation, $bill_payable_has_tasa],
             'amount' => $total_rules,
             'date' => ['required', 'date_format:Y-m-d', 'before_or_equal:' . Carbon::now()->format('Y-m-d')],
         ];
